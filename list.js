98e59501afc0488629f47f0cc93ac4ed50349772
bob-perez-utah <bperez@gw.novell.com>
2018-08-29 19:48:46 -0600

    final commit

---
+++ list.js
@@ -0,0 +1,23 @@
+window.setTimeout(function() {
+
+var todos = ["Buy New Turtle"];
+
+var input = prompt("What would you like to do ? :");
+
+while (input !== "quit") {
+    // handle input
+    if (input === "list") {
+        console.log(todos);
+    } else if (input === "new") {
+        // ask for new todo
+        var newTodo = prompt("Enter new Todo");
+        todos.push(newTodo);
+    }
+
+    //ask again for new input
+    input = prompt("What would you like to do ? :");
+}
+
+  console.log("OK, You Quit The App");
+    
+}, 500);
